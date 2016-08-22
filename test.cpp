f0e7bd1e2550186341cf1810175e57e3084b5e35
xgerrmann <xander@gerrmann.nl>
2016-08-22 13:47:28 +0200

    Actually opencv 3 is used (not 2.4).

--- test.cpp
+++ test.cpp
@@ -1,43 +1,29 @@
 #include <iostream>
-using namespace std;
-#include "opencv2/core.hpp"
-#include "opencv2/core/utility.hpp"
-#include "opencv2/cudabgsegm.hpp"
-//#include "opencv2/cudalegacy.hpp"
-#include "opencv2/video.hpp"
-//#include "opencv2/opencv.hpp"
-//include "opencv2/gpu/gpu.hpp"
+
+#include <vector>
+
+using namespace std; // place before opencv2/gpu/gpu
+
 #include "opencv2/opencv.hpp"
+#include "opencv2/core.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/gpu/gpu.hpp"
+#include "opencv2/imgcodecs.hpp"
 
+using namespace cv;
 
-
-//using namespace cv;
 int main (int argc, char* argv[])
 {
     try
     {
-        cv::Mat src_host = cv::imread("file.png", CV_LOAD_IMAGE_GRAYSCALE);
-//        cv::gpu::GpuMat dst, src;
+		std::string fname = "image.png";
+		cv::Mat src_host = imread(fname, IMREAD_GRAYSCALE);
+		cv::cuda::GpuMat dst, src;
 //        src.upload(src_host);
-//
-//        cv::gpu::threshold(src, dst, 128.0, 255.0, CV_THRESH_BINARY);
-//
-//        cv::Mat result_host;
-//        dst.download(result_host);
-//
+//        cv::gpu::threshold(src, dst, 128.0, 255.0, THRESH_BINARY);
+//        cv::Mat result_host(dst);
 //        cv::imshow("Result", result_host);
 //        cv::waitKey();
-
-		cv::cuda::GpuMat dst, src;
-        src.upload(src_host);
-
-        cv::cuda::threshold(src, dst, 128.0, 255.0, CV_THRESH_BINARY);
-
-        cv::Mat result_host;
-        dst.download(result_host);
-
-        cv::imshow("Result", result_host);
-        cv::waitKey();
     }
     catch(const cv::Exception& ex)
     {
